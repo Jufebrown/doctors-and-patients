7058ad6452d5f55fa8159b417518e6d431cfe051
Jufebrown <jufebrown@gmail.com>
2017-01-20 15:53:41 -0600

    deleted unused item

--- app/app.js
+++ app/app.js
@@ -29,7 +29,6 @@ app.controller('DetailCtrl', function($scope, $routeParams, $http) {
   $http.get(`list.json`)
   .then(function(val) {
     $scope.selectedItem = val.data.list[$scope.thatNumber]
-    // $scope.selectedItem = list[$scope.thatNumber]
   })
 })
 
